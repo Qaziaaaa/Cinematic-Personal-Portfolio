a8d82cc7f287e8d7cda74362c67eb4a63b9fd7aa
Qazi Ahmad <qazithekingston@gmail.com>
2025-12-23 16:38:43 +0000

    Try fixing this error: `Runtime Error: [object Event]. Error source: Cal

--- src/components/ParallaxHero.tsx
+++ src/components/ParallaxHero.tsx
@@ -139,7 +139,7 @@ export default function ParallaxHero({ onProgress, onLoaded }: ParallaxHeroProps
       
       if (newFrameIndex !== frameIndex.current) {
         frameIndex.current = newFrameIndex;
-        requestAnimationFrame(() => drawFrame(newFrameIndex));
+        requestAnimationFrame(() => drawFrame(frameIndex.current));
       }
     };
     
