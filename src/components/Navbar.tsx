c663c42c84179c17a7622990dfd510e2f3715a07
Qazi Ahmad <qazithekingston@gmail.com>
2025-12-27 15:22:40 +0000

    make the logo size bigger

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,10 +15,10 @@ const navLinks = [
 
 function Logo() {
     return (
-        <div className="flex items-center font-headline text-xl font-bold">
-            <span className="text-primary font-bold text-2xl">{'{'}</span>
+        <div className="flex items-center font-headline text-2xl font-bold">
+            <span className="text-primary font-bold text-3xl">{'{'}</span>
             <span className="mx-1 uppercase">Qazi</span>
-            <span className="text-primary font-bold text-2xl">{'}'}</span>
+            <span className="text-primary font-bold text-3xl">{'}'}</span>
         </div>
     )
 }
